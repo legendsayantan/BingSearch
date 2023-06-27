6edc184f1531c09020e1beb032d53c50b83bb7e0
legendsayantan <legendsayantanpaul@gmail.com>
2023-06-27 23:27:23 +0530

    updated python code to auto-close tabs

--- python/search.py
+++ python/search.py
@@ -3,13 +3,20 @@ import string
 import subprocess
 import time
 
+edge_path = r"C:\Users\Admin\AppData\Local\Microsoft\Edge\Application\msedge.exe"  # Replace with the actual Edge executable path
+count = 30 # How many searches to make
+delay_seconds = 3 # Replace with actual delay
+close_tabs = True # If you want to close the tabs immediately (True/False)
+
 words = []
-for i in range(30):
+for i in range(count):
     word = ''.join(random.choice(string.ascii_lowercase) for j in range(random.randint(5, 10)))
     words.append(word)
 
-edge_path = r"C:\Users\Admin\AppData\Local\Microsoft\Edge\Application\msedge.exe"  # Replace with the actual Edge executable path
 for word in words:
     search_url = f'https://www.bing.com/search?q={word}'
-    subprocess.Popen([edge_path, search_url])
-    time.sleep(3)
+    edge_process = subprocess.Popen([edge_path, search_url])
+    time.sleep(delay_seconds)
+    if close_tabs:
+        edge_process.terminate()
+        edge_process.wait()
